1d7f3f96ac821ef2f7c3c311f95b47241a09b14f
Vevota <ryananjain1@gmail.com>
2026-06-01 04:40:26 -0700

    Force OSC visibility always, set osd-level

--- player.c
+++ player.c
@@ -36,6 +36,8 @@ struct player *player_create(const char *url, int width, int height) {
     mpv_set_option_string(pl->mpv, "audio-display", "no");
     mpv_set_option_string(pl->mpv, "osc", "yes");
     mpv_set_option_string(pl->mpv, "load-scripts", "yes");
+    mpv_set_option_string(pl->mpv, "osc-visibility", "always");
+    mpv_set_option_string(pl->mpv, "osd-level", "1");
     mpv_set_option_string(pl->mpv, "terminal", "no");
 
     if (mpv_initialize(pl->mpv) < 0) {
